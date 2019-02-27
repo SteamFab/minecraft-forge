d019e05b072768e3876661d4714035e87245d9c8
Martin Steinmann <martin@caliviaconsult.com>
2019-02-27 10:32:09 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,9 +11,9 @@ rsyslogd &
 cd /home/$NB_USER
 cp -r mods server/           # copy mods that were installed via Dockerfile
 cp -r libraries server/
-cp forge-$VERSION-universal.jar server/
+cp forge-$VERSION.jar server/
 cp minecraft_server*.jar server/
 chown -R $NB_USER:users *
 
 #start minecraft w/ user minecraft
-su - $NB_USER -c "cd server; java -Xms1G -Xmx7G -d64 -jar forge-$VERSION-universal.jar"
+su - $NB_USER -c "cd server; java -Xms1G -Xmx7G -d64 -jar forge-$VERSION.jar"
